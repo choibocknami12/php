d080aaf14b30e434f5c60e035bc072d1123133b1
choibocknami12 <h_hcho1215@naver.com>
2023-09-14 23:06:31 +0900

    0914_home

--- php/tng/88_02_rsp.php
+++ php/tng/88_02_rsp.php
@@ -4,8 +4,8 @@
 // echo "입력값 : {$sin_str}"; 
 
 
-$user = rand(1, 3);
-$num = rand(1, 3);
+// $user = rand(1, 3);
+// $num = rand(1, 3);
 //echo $num;
 
     // if ($num === 1) {
@@ -18,17 +18,76 @@ $num = rand(1, 3);
     //     echo "보";
     // }
 
-switch($num) {
-    case 1:
-        echo "C";
-    break;
-    case 2:
-        echo "R";
-    break;
-    case 3:
-        echo "P";
-    break;
-}    
+// switch($num) {
+//     case 1:
+//         echo "C";
+//     break;
+//     case 2:
+//         echo "R";
+//     break;
+//     case 3:
+//         echo "P";
+//     break;
+// }    
+
+// $num = rand(1,3);
+// $user = 2;
+// if($num===1){
+//         echo "R";
+//         if($user===1){
+//             echo "lose";
+//         }
+//         else if($user===2){
+//             echo "draw";
+//         }
+//         else if($user===3){
+//             echo "win";
+//         }
+//     }
+//     if($num===2){
+//         echo "S";
+//         if($user===1){
+//             echo "win";
+//         }
+//         else if($user===2){
+//             echo "draw";
+//         }
+//         else if($user===3){
+//             echo "lose";
+//         }
+//     }
+//     if($num===3){
+//         echo "P";
+//         if($user===1){
+//             echo "lose";
+//         }
+//         else if($user===2){
+//             echo "win";
+//         }
+//         else if($user===3){
+//             echo "defeat";
+//         }
+//     }
+
+$user = 2;
+$com = rand(1,3);
+
+for (?){}
+
+if($user===$com){
+    echo "draw";
+}
+else if(($user===1 && $com===2)
+    ||($user===2 && $com===3)
+    ||($user===3 && $com===1)){
+        echo "win";
+    }
+else if(($user===1 && $com===3)
+    ||($user===2 && $com===1)
+    ||($user===3 && $com===2)){
+        echo "lose";
+    }
+
 
 
 
