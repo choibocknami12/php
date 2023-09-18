82f860e9789d7602cae40d14f903ff02a4852627
choibocknami12 <h_hcho1215@naver.com>
2023-09-18 22:24:12 +0900

    0918_homework

--- php/tng/88_04_PDO.php
+++ php/tng/88_04_PDO.php
@@ -8,48 +8,136 @@
 
 //1.
 
+// require_once("../ex/04_107_fnc_db_connect.php");
+// $conn = null;
+// my_db_conn( $conn );
+
+// $sql = 
+//     " INSERT INTO "
+//     ." employees ( "
+//     ."  emp_no   "
+//     ." ,birth_date "
+//     ." ,first_name "
+//     ." ,last_name "
+//     ." ,gender "
+//     ." ,hire_date "
+//     ." ) "
+//     ." VALUES ( "
+//     ." :emp_no "
+//     ." ,:birth_date "
+//     ." ,:first_name "
+//     ." ,:last_name "
+//     ." ,:gender "
+//     ." ,:hire_date "
+//     ." ) "
+//     ;
+
+// $arr_ps = [
+//     ":emp_no" => 500001
+//     ,":birth_date" => 19930624
+//     ,":first_name" => "hyunhee"
+//     ,":last_name" => "Choi"
+//     ,":gender" => "F"
+//     ,":hire_date" => 20230918
+// ];
+
+// $stmt = $conn -> prepare($sql);
+// $result = $stmt->execute($arr_ps);
+// $conn->commit();
+// print_r($result);
+
+//2.
+
+// require_once("../ex/04_107_fnc_db_connect.php");
+// $conn = null;
+// my_db_conn( $conn );
+
+// $sql = 
+//     " UPDATE "
+//     ."employees"
+//     ." SET "
+//     ."first_name = :first_name"
+//     ." WHERE "
+//     ."emp_no = 500001"
+//     ;
+
+// $arr_ps = [
+//     ":first_name" => "호이"
+// ];    
+
+//     $stmt = $conn -> prepare($sql);
+//     $result = $stmt->execute($arr_ps);
+//     $conn->commit();
+//     print_r($result);
+//------------------------------------------------------
+    // require_once("../ex/04_107_fnc_db_connect.php");
+    // $conn = null;
+    // my_db_conn( $conn );
+    
+    // $sql = 
+    //     " UPDATE "
+    //     ."employees"
+    //     ." SET "
+    //     ."last_name = :last_name"
+    //     ." WHERE "
+    //     ."emp_no = 500001"
+    //     ;
+    
+    // $arr_ps = [
+    //     ":last_name" => "둘리"
+    // ];    
+    
+    //     $stmt = $conn -> prepare($sql);
+    //     $result = $stmt->execute($arr_ps);
+    //     $conn->commit();
+    //     print_r($result);
+
+//3.
+
+// require_once("../ex/04_107_fnc_db_connect.php");
+// $conn = null;
+// my_db_conn( $conn );
+
+// $sql =
+//     " SELECT "
+//     ." * "
+//     ." FROM " 
+//     ."employees"
+//     ." WHERE "
+//     ."emp_no = :emp_no";
+
+// $arr_ps = [
+//     ":emp_no" => 500001
+// ];
+
+// $stmt = $conn->prepare($sql);
+// $stmt -> execute($arr_ps); // 쿼리 실행
+// $result = $stmt->fetchALL(); // 쿼리 결과를 fetch
+// print_r($result);
+// $conn = null;
+
+//4.
+
 require_once("../ex/04_107_fnc_db_connect.php");
 $conn = null;
 my_db_conn( $conn );
 
-$sql = 
-    " INSERT INTO "
-    ." employees ( "
-    ."  emp_no   "
-    ." ,birth_date "
-    ." ,first_name "
-    ." ,last_name "
-    ." ,gender "
-    ." ,hire_date "
-    ." ) "
-    ." VALUES ( "
-    ." :emp_no "
-    ." ,:birth_date "
-    ." ,:first_name "
-    ." ,:last_name "
-    ." ,:gender "
-    ." ,:hire_date "
-    ." ) "
-    ;
+$sql =
+" DELETE FROM " 
+." employees "
+." WHERE "
+." emp_no = :emp_no "
+;
 
 $arr_ps = [
     ":emp_no" => 500001
-    ,":birth_date" => 19930624
-    ,":first_name" => "hyunhee"
-    ,":last_name" => "Choi"
-    ,":gender" => "F"
-    ,":hire_date" => 20230918
 ];
 
-// $stmt = $conn -> prepare($sql);
-// $stmt->execute($arr_ps);
-// $result = $stmt -> fetchALL();
 $stmt = $conn -> prepare($sql);
 $result = $stmt->execute($arr_ps);
 $conn->commit();
 print_r($result);
-
-
+//rowCount사용해야하나? 꼭? >> 물어보기.
 
 
 ?>
