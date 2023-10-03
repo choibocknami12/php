4c9530ece4c40171a56fdb4adbb9987c9593c3f0
choibocknami12 <h_hcho1215@naver.com>
2023-10-04 01:08:57 +0900

    1004

---
+++ side_project/mini_test/src/list.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="MINI_TEST/src/css/common.css">
+    <title>리스트페이지</title>
+</head>
+<body>
+    
+</body>
+</html>
