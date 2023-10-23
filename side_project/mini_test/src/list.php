27822df82eae0377ae7619ec4548b711e02a2fac
choibocknami12 <h_hcho1215@naver.com>
2023-10-24 00:38:19 +0900

    mini_test

--- side_project/mini_test/src/list.php
+++ side_project/mini_test/src/list.php
@@ -2,7 +2,7 @@
 define("ROOT", $_SERVER["DOCUMENT_ROOT"]."/mini_test/src");
 
 
-?>
+?> 
 
 <!DOCTYPE html>
 <html lang="ko">
@@ -14,11 +14,32 @@ define("ROOT", $_SERVER["DOCUMENT_ROOT"]."/mini_test/src");
     <title>리스트페이지</title>
 </head>
 <body>
-    <div class="header">
-        <h1><a href="">최복나미 일기</a></h1>
+    <div class="header text_align">
+        <h1><a href="">최복나미 체크리스트</a></h1>
     </div>
-    <form action="">
-        <label for=""></label>
-    </form>
+    <div class="main text_align">
+        <div class="list">
+            <div>
+                <input type="checkbox" id="list_1">
+                <label for="list_1">화장실 교체</label>
+            </div>
+            <div>
+                <input type="checkbox" id="list_2">
+                <label for="list_2">물그릇 교체</label>
+            </div>
+            <div>
+                <input type="checkbox" id="list_3">
+                <label for="list_3">놀아주기</label>
+            </div>
+            <div>
+                <input type="checkbox" id="list_4">
+                <label for="list_4">사료확인</label>
+            </div>
+            <div>
+                <input type="checkbox" id="list_5">
+                <label for="list_5">털빗기</label>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
