5a48b38bdd7bd4ff93c2f48b71bcb9bf9f0449cd
choibocknami12 <h_hcho1215@naver.com>
2023-10-23 18:09:27 +0900

    mini_test

--- side_project/mini_test/src/list.php
+++ side_project/mini_test/src/list.php
@@ -1,4 +1,6 @@
 <?php
+define("ROOT", $_SERVER["DOCUMENT_ROOT"]."/mini_test/src");
+
 
 ?>
 
@@ -7,10 +9,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="MINI_TEST/src/css/common.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poor+Story&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/mini_test/src/css/common.css">
     <title>리스트페이지</title>
 </head>
 <body>
-    
+    <div class="header">
+        <h1><a href="">최복나미 일기</a></h1>
+    </div>
+    <form action="">
+        <label for=""></label>
+    </form>
 </body>
 </html>
