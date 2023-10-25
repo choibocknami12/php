94c5bb922c4b5e7cd8d088bf7fe949220fd85cca
choibocknami12 <h_hcho1215@naver.com>
2023-10-26 00:57:13 +0900

    1026commitㅅㅂ

--- side_project/homework/src/lib/db_lib.php
+++ side_project/homework/src/lib/db_lib.php
@@ -34,7 +34,82 @@ try {
     } 
 }
 
+//------------------------------------------
+// 함수명   : db_destroy_conn
+// 기능     : DB connect
+// 파라미터 : PDO &$conn
+// 리턴     : 없음
+// -----------------------------------------
+function db_destroy_conn(&$conn) {
+    $conn = null; 
+}
 
+//------------------------------------------
+// 함수명   : db_select_boards_paging
+// 기능     : boards pasging 조회
+// 파라미터 : PDO  &$conn
+// 리턴     : Int / false
+// -----------------------------------------
+
+function db_select_boards_cnt(&$conn) {
+    $sql =
+        " SELECT "
+        ." COUNT(id) as cnt"
+        ." FROM "
+        ." boards "
+        ." WHERE "
+        ." delete_date is null "
+        ;
+        try {
+            $stmt = $conn->query($sql);  // 쿼리 준비 실행 다같이 해줌. stmt에 값을 받을게 없으니까!
+            $result = $stmt->fetchAll();
+//var_dump($result);
+            return (int)$result[0]["cnt"];// 정상일 때 쿼리결과ㅑ 리턴
+        } catch(Exception $e) {
+            return false; 
+        }
+}
+
+//------------------------------------------
+// 함수명   : db_select_boards_paging
+// 기능     : boards pasging 조회
+// 파라미터 : PDO  &$conn
+//            Array &$arr_param
+// 리턴     : Array / false
+// -----------------------------------------
+
+function db_select_boards_pasging(&$conn, &$arr_param) {
+    try {
+        $sql =
+        " SELECT "
+        ." id "
+        ." ,title "
+        ." ,create_date "
+        ." FROM "
+        ." boards "
+        ." WHERE "
+        ." delete_date IS NULL "
+        ." ORDER BY "
+        ." id DESC "
+        ." LIMIT :list_cnt OFFSET :offset "
+        ;
+
+        $arr_ps = [
+            ":list_cnt" => $arr_param["list_cnt"]
+            ,":offset" => $arr_param["offset"]
+        ];
+
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($arr_ps);
+        $result = $stmt->fetchAll();
+
+        return $result;
+
+    } catch(Exception $e){
+        echo $e->getMessage();
+        return false;
+    }
+}
 
 
 ?>
--- side_project/homework/src/list.php
+++ side_project/homework/src/list.php
@@ -1,6 +1,71 @@
 <?php
 define("ROOT", $_SERVER["DOCUMENT_ROOT"]."/homework/src");
-//require_once(ROOT."lib/db_lib.php");
+require_once(ROOT."/lib/db_lib.php");
+
+$conn = null;
+
+$list_cnt = 3;
+$page_num = 1; 
+
+try {
+    if(!my_conn($conn)) {
+        throw new Exception("DB Error : PDO Instance");
+    }
+
+    $boards_cnt = db_select_boards_cnt($conn);
+    if($boards_cnt === false) {
+        throw new Exception("DB Error : SELECT Count");
+    }
+
+    $max_page_num = ceil($boards_cnt / $list_cnt); //최대 페이지 수
+
+    if(isset($_GET["page"])) {
+        $page_num = $_GET["page"]; // 유저가 보내온 페이지 셋팅
+    }
+    
+    //var_dump($_GET["page"]);
+    $offset = ($page_num - 1) * $list_cnt; //offset계산
+
+    //이전버튼
+    $prev_page_num = $page_num - 1;
+    if($prev_page_num === 0) {
+        $prev_page_num = 1;
+    }
+
+    //다음버튼
+    $next_page_num = $page_num + 1;
+    if($next_page_num > $max_page_num) {
+        $next_page_num = $max_page_num;
+    }
+
+    // DB조회시 사용할 데이터 배열
+    $arr_param = [
+        "list_cnt" => $list_cnt
+        ,"offset" => $offset
+    ];
+
+    //게시글 리스트 조회
+    $result = db_select_boards_pasging($conn, $arr_param);
+    
+    //게시글 조회 에러
+    if(!$result) {
+        throw new Exception("DB Error : SELECT boards Paging");
+    }
+
+}
+catch(Exception $e) {
+    
+    echo $e->getMessage(); //v002 del
+    //header("Location: error.php/?err_msg={$e->getMessage()}");
+    exit;
+}
+finally {
+    db_destroy_conn($conn);
+}
+
+
+db_destroy_conn($conn);
+
 
 ?>
 
@@ -23,41 +88,37 @@ define("ROOT", $_SERVER["DOCUMENT_ROOT"]."/homework/src");
         <table class="text_align">
             <colgroup>
                 <col width = "20%">
-                <col width = "50%">
-                <col width = "30%">
+                <col width = "60%">
+                <col width = "20%">
             </colgroup>
             <tr>
                 <th class="list_title">번호</th>
                 <th class="list_title">제목</th>
                 <th class="list_title">작성일자</th>
             </tr>
+            <?php
+                foreach($result as $item) {
+            ?>
             <tr>
-                <td>아이우에오</td>
-                <td>카키쿠케고</td>
-                <td>사시수세소</td>
+                <td><?php echo $item["id"]; ?></td>
+                <td>
+                    <a href="/homework/src/detail.php/?id=<?php echo $item["id"]; ?>&page=<?php echo $page_num; ?>">
+                        <?php echo $item["title"]; ?>
+                    </a>
+                </td>
+                <td><?php echo $item["create_date"]; ?></td>
             </tr>
-            <tr>
-                <td>아이우에오</td>
-                <td>카키쿠케고</td>
-                <td>사시수세소</td>
-            </tr>
-            <tr>
-                <td>아이우에오</td>
-                <td>카키쿠케고</td>
-                <td>사시수세소</td>
-            </tr>
+            <?php } ?>
         </table>
     </div>
 
     <div class="btn text_align">
         <section>
-            <a href="">이전</a>
-            <a href="">1</a>
-            <a href="">2</a>
-            <a href="">3</a>
-            <a href="">다음</a>
+            <a href="/homework/src/list.php/?page=<?php echo $prev_page_num ?>">이전</a>
+            <a class="page_btn <?php echo $str ?>" href="/mini_board/src/list.php/?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+            <a href="/homework/src/list.php/?page=<?php echo $next_page_num ?>">다음</a>
         </section>
     </div>
-        <a href="" class="text_align">작성하기</a>
+        <a href="/homework/src/insert.php" class="text_align">작성하기</a>
 </body>
 </html>
