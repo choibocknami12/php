175d1893079e3fdcba50090b73eed4e43761d890
choibocknami12 <h_hcho1215@naver.com>
2023-10-30 18:08:12 +0900

    1030home

--- side_project/homework/src/update.php
+++ side_project/homework/src/update.php
@@ -1,4 +1,9 @@
 <?php
+define("ROOT", $_SERVER["DOCUMENT_ROOT"]."/homework/src");
+define("ERROR_MSG_PARAM", "%s : 필수 입력 사항입니다.");
+require_once(ROOT."/lib/db_lib.php");
+
+
 
 ?>
 
@@ -7,6 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/homework/src/css/common.css">
     <title>update_page</title>
 </head>
 <body>
@@ -22,16 +28,19 @@
             </tr>
             <tr>
                 <th>제목</th>
-                <td><input type="text" name="title" value="<?php echo $item["id"]; ?>"></td>
+                <td><input type="text" name="title" value="<?php echo $item["title"]; ?>"></td>
             </tr>
             <tr>
-                <th>글번호</th>
-                <td><?php echo $item["id"]; ?></td>
+                <th>내용</th>
+                <td><textarea name="memo" id="memo" cols="30" rows="10"><?php echo $item["memo"]?></textarea></td>
             </tr>
 
         </table>
     </form>
-
+        <div>
+            <button type="submit">수정</button>
+            <a href="/homework/src/list.php/?page=<?php echo $id; ?>&page=<?php echo $page; ?>">취소</a>
+        </div>
     
 </body>
 </html>
