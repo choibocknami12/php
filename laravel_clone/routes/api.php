323994c247985cb906f5dfee1bd2bb28b6f5e29c
choibocknami12 <h_hcho1215@naver.com>
2023-11-25 13:32:41 +0900

    1125라라벨연습

--- laravel_clone/routes/api.php
+++ laravel_clone/routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Route::get('/main', function () { // 경로지정. http://127.0.0.1:8000/api/main
+//     return view('main'); // view의 main페이지 띄워라
+// });
+
+// 파라미터가 있는 형태 >>파라미터가 없으면 주소창에서 연결안됨
+// 파라미터에 null 추가하면 빈값으로도 가능함.
+Route::get('/users/{name}', function($name = null) {
+    return '안녕,'.$name;
+});
