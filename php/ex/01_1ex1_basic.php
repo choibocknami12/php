89a52a1922cee7ce09b59fb73db66878a898ede8
choibocknami12 <h_hcho1215@naver.com>
2024-03-13 00:08:05 +0900

    0313별찍기

--- php/ex/01_1ex1_basic.php
+++ php/ex/01_1ex1_basic.php
@@ -24,23 +24,50 @@
 //    $i++;
 //  }
 
- $base = 5;
- $line = $base;
- while ($line >= 1) {
-    $space = $line - 1;
-    $one_line = $base;
-    while($one_line >= 1) {
-      if($space >= 1) {
-        echo " ";
-      } else {
-        echo "*";
-      }
-      $space--;
-      $one_line--;
-    }
-    echo "\n";
-    $line--;
- }
- 
+// 	$base = 5;
+// 	$line = $base;
+
+// 		while ($line >= 1) {
+//     $space = $line - 1;
+//     $one_line = $base;
+
+// 	while($one_line >= 1) {
+// 		if($space >= 1) {
+// 			echo " ";
+// 		} else {
+// 			echo "*";
+// 		}
+// 		$space--;
+// 		$one_line--;
+// 	}
+//     echo "\n";
+//     $line--;
+// }
+
+
+// for($i = 1; $i <= 5; $i++) {
+// 	for($j = 0; $j <= 5; $j++) {
+// 		if($j < $i) {
+// 			echo "*";
+// 		}
+// 	}
+// 	echo "\n";
+// 	// echo "*";
+// }
+
+for($i = 0; $i <= 5; $i++) {
+	for($j = 0; $j < $i; $j++) {
+		echo "*";
+	}
+	echo "\n";
+}
+
+for($a = 0; $a <= 5; $a++) {
+	for($b = 5; $b > $a; $b--) {
+		echo "*";
+	}
+	echo "\n";
+}
+
 
 ?>
