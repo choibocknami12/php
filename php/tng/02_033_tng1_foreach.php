62c39350f25160188519966bd9e364a926aeb966
choibocknami12 <h_hcho1215@naver.com>
2024-03-15 13:15:31 +0900

    0315

--- php/tng/02_033_tng1_foreach.php
+++ php/tng/02_033_tng1_foreach.php
@@ -1,23 +1,62 @@
 <?php
-$arr = [
-    [
-        "id" => "meerkat1"
-        ,"pw" => "php504"
-    ]
-    ,[
-        "id" => "meerkat2"
-        ,"pw" => "php504"
-    ]
-    ,[
-        "id" => "meerkat3"
-        ,"pw" => "php504"
-    ]
-];
+// $arr = [
+//     [
+//         "id" => "id list"
+//         ,"pw" => "pw list"
+//     ],
+//     [
+//         "id" => "meerkat1"
+//         ,"pw" => "php504"
+//     ]
+//     ,[
+//         "id" => "meerkat2"
+//         ,"pw" => "php504"
+//     ]
+//     ,[
+//         "id" => "meerkat3"
+//         ,"pw" => "php504"
+//     ]
+// ];
 
-foreach($arr as $item) {
-    echo $item["id"]."\n";
+// foreach($arr as $item) {
+//     echo $item["id"]."\n";
+// }
+
+// echo "id list";
+
+$arr = [2, 3, 5, 50, 15, 60];
+
+// echo $arr[0];
+// $min = $max = $arr[0];
+
+// $min = 0;
+// $max = 0;
+$max = $arr[0];
+$min = $arr[0];
+
+foreach($arr as $num) {
+    if($max < $num) {
+        $max = $num;
+    }
+    if($min > $num) {
+        $min = $num;
+    }
 }
+echo $max.$min;
 
-echo "id list";
+// if($min ) {
+    // for($i = 0; $i < count($arr); $i++) {
+    //     if($max < $arr[$i]) {
+    //         $max = $arr[$i];
+    //     }
+    //     if($min > $arr[$i]) {
+    //         $min = $arr[$i];
+    //     }
+    // }
+// }    
+
+// echo "최소값: " . $min;
+// echo "\n";
+// echo "최대값: " . $max;
 
 ?>
